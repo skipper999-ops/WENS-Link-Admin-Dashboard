fe53979a6be57c51400656c5aa31c4da9ac39501
dhirusarania <dhirusarania@gmail.com>
2019-10-26 07:47:53 +0530

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,75 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script:[
+      {
+        src: "https://code.jquery.com/jquery-3.4.1.min.js",
+        type: "text/javascript"
+      },
+      {
+        src: "https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js",
+        type: "text/javascript"
+      },
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    "~static/files/css/framework.css"
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: '~/plugins/PerfectScrollbar', lang: 'js' },
+    { src: '~/plugins/goodtable', lang: 'js' }
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    'cookie-universal-nuxt'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/dashboard/templates/specification/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="navbar-spacing padding-top-30">
+    <div class="specification">
+      <div class="holder">
+        <div
+          class="column-padding header-bottom"
+          style="display: flex; justify-content: space-between"
+        >
+          <h3 style="display: flex;align-items: center;">Specification Template</h3>
+          <!-- <button class="btn btn-red" style="display: flex;align-items: center;">
+            <i data-feather="upload"></i>
+            <p class="padding-left-10 white-text">Upload New</p>
+          </button>-->
+        </div>
+
+        <div class="row">
+          <vue-good-table :columns="columns" :rows="rows">
+            <template slot="table-row" slot-scope="props">
+              <span v-if="props.column.field === 'details'">
+                <button type="button" class="btn btn-primary">EDIT</button>
+              </span>
+              <span v-else>{{ props.formattedRow[props.column.field] }}</span>
+            </template>
+          </vue-good-table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-component",
+  data() {
+    return {
+      columns: [
+        {
+          label: "Category",
+          field: "name"
+        },
+        {
+          label: "Age",
+          field: "age",
+          type: "number"
+        },
+        {
+          label: "Created On",
+          field: "createdAt",
+          type: "date",
+          dateInputFormat: "yyyy-MM-dd",
+          dateOutputFormat: "MMM Do yy"
+        },
+        {
+          label: "Action",
+          field: "details"
+        }
+      ],
+      rows: [
+        {
+          id: 1,
+          name: "Mobile",
+          age: 20,
+          createdAt: "2011-10-31",
+          details: "<p>asa</p>"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+
+<style>
+
+</style>
